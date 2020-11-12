4340b5659657a82792ad9ccede9739024eeb4890
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-11-12 20:40:16 +0100

    Populated DOM

--- scripts/main.js
+++ scripts/main.js
@@ -5,20 +5,105 @@ const toggleNav = () => {
   nav.classList.toggle('open');
 };
 
-const populateDOM = data => {
+
+const createRepository = (repository) => {
+  console.log(repository);
+  const link = document.createElement('a');
+  link.setAttribute('href', `https://github.com${repository.resourcePath}`);
+  link.textContent = repository.name;
+
+  const description = document.createElement('p');
+  description.classList.add('repository__description');
+  description.textContent = repository.description;
+
+  const updated = document.createElement('p');
+  updated.classList.add('updated');
+  updated.textContent = `updated ${format(new Date(repository.updatedAt))}`;
+
+  // Repository Language
+  const language = document.createElement('div');
+  language.classList.add('language');
+
+  const languageName = document.createElement('span');
+  languageName.textContent = repository.primaryLanguage && repository.primaryLanguage.name;
+
+  const languageColor = document.createElement('div');
+  languageColor.classList.add('language__color');
+  languageColor.style.backgroundColor = repository.primaryLanguage && repository.primaryLanguage.color;
+
+  language.append(languageColor, languageName);
+  // End of repository language
+
+  // Star counts
+  const starCount = document.createElement('span');
+  starCount.classList.add('count', 'count--star');
+
+  const starImg = document.createElement('img');
+  starImg.setAttribute('src', '/images/star.svg');
+
+  const starNum = document.createElement('p');
+  starNum.textContent = repository.stargazerCount;
+
+  starCount.append(starImg, starNum);
+  // End of star count
+
+  // Fork counts
+  const forkCount = document.createElement('span');
+  forkCount.classList.add('count', 'count--fork');
+
+  const forkImg = document.createElement('img');
+  forkImg.setAttribute('src', '/images/fork.svg');
+
+  const forkNum = document.createElement('p');
+  forkNum.textContent = repository.forkCount;
+
+  forkCount.append(forkImg, forkNum);
+  // End of fork count
+
+  const repoDetails = document.createElement('div');
+  repoDetails.classList.add('repository__details');
+
+  if (repository.primaryLanguage) {
+    repoDetails.appendChild(language);
+  }
+
+  if (repository.stargazerCount) {
+    repoDetails.appendChild(starCount);
+  }
+
+  if (repository.forkCount) {
+    repoDetails.appendChild(forkCount);
+  }
+  repoDetails.appendChild(updated);
+
+  const div = document.createElement('div');
+  div.append(link, description, repoDetails);
+
+  const btn = document.createElement('btn');
+  btn.classList.add('repository__btn');
+  btn.innerHTML = `<img src="./images/star.svg" alt=""> Star`;
+
+  const repositoryContainer = document.createElement('li');
+  repositoryContainer.classList.add('repository');
+  repositoryContainer.append(div, btn);
+
+  return repositoryContainer
+};
+
+const populateDOM = (data) => {
   console.log(data);
   const profileImage = document.querySelector('.profile-img');
   profileImage.setAttribute('src', data.avatarUrl);
   profileImage.setAttribute('alt', `${data.name} profile image`);
-  
+
   const avatarImage = document.querySelector('.avatar__image');
   avatarImage.setAttribute('src', data.avatarUrl);
   avatarImage.setAttribute('alt', `${data.name} profile image`);
-  
+
   const avatarImageLarge = document.querySelector('.user__profile img');
   avatarImageLarge.setAttribute('src', data.avatarUrl);
   avatarImageLarge.setAttribute('alt', `${data.name} profile image`);
-  
+
   const avatarName = document.querySelector('.avatar__name');
   const userName = document.querySelector('.user__name');
   avatarName.textContent = data.login;
@@ -30,25 +115,30 @@ const populateDOM = data => {
   const userBio = document.querySelector('.user__bio');
   userBio.textContent = data.bio;
 
-  const repositories = data.repositories.edges;
-
   const repoCount = document.querySelector('#repo-count');
   repoCount.textContent = data.repositories.totalCount;
-  
-  
-  
-  
+
+  const repositories = data.repositories.edges;
+
+  const fragment = document.createDocumentFragment();
+  repositories.forEach(({ node }) => {
+    fragment.appendChild(createRepository(node));
+  });
+
+  const repositoriesContainer = document.querySelector('.repositories');
+  repositoriesContainer.append(fragment);
+
   loader.style.display = 'none';
-}
+};
 
 loader.style.display = 'flex';
 const githubData = {
   token: 'ef7f55a69a526c830119bf634ccc823338f03ab3',
   username: 'ireade',
-}
+};
 
 const body = {
-  'query': `
+  query: `
   query { 
     user(login: "${githubData.username}") { 
       bio
@@ -75,26 +165,27 @@ const body = {
       }
     }
   }
-  `
-}
+  `,
+};
 
 const fetchData = {
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `bearer ${githubData.token}`
+    Authorization: `bearer ${githubData.token}`,
   },
   body: JSON.stringify(body),
-  method: 'POST'
+  method: 'POST',
 };
 
 fetch('https://api.github.com/graphql', fetchData)
-.then(res => res.json())
-.then(({ data: { user } }) => {
-  populateDOM(user);
-}).catch((err) => {
-  console.log(err);
-  loader.style.display = 'none';
-})
+  .then((res) => res.json())
+  .then(({ data: { user } }) => {
+    populateDOM(user);
+  })
+  .catch((err) => {
+    console.log(err);
+    loader.style.display = 'none';
+  });
 
 const handleScroll = () => {
   const image = document.querySelector('.user__profile img');
@@ -108,7 +199,7 @@ const handleScroll = () => {
     avatar.classList.remove('show');
     mainHeader.style.zIndex = '0';
   }
-}
+};
 
 let debounce;
 window.addEventListener('scroll', () => {
