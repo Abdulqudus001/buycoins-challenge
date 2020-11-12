303f2b464d2afce0b3845341e5052a6b2b68c25c
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-11-12 19:31:42 +0100

    Added stars and fork count

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector('.nav');
+const loader = document.querySelector('.loader');
 
 const toggleNav = () => {
   nav.classList.toggle('open');
@@ -6,8 +7,41 @@ const toggleNav = () => {
 
 const populateDOM = data => {
   console.log(data);
+  const profileImage = document.querySelector('.profile-img');
+  profileImage.setAttribute('src', data.avatarUrl);
+  profileImage.setAttribute('alt', `${data.name} profile image`);
+  
+  const avatarImage = document.querySelector('.avatar__image');
+  avatarImage.setAttribute('src', data.avatarUrl);
+  avatarImage.setAttribute('alt', `${data.name} profile image`);
+  
+  const avatarImageLarge = document.querySelector('.user__profile img');
+  avatarImageLarge.setAttribute('src', data.avatarUrl);
+  avatarImageLarge.setAttribute('alt', `${data.name} profile image`);
+  
+  const avatarName = document.querySelector('.avatar__name');
+  const userName = document.querySelector('.user__name');
+  avatarName.textContent = data.login;
+  userName.textContent = data.name;
+
+  const userAccount = document.querySelector('.user__account');
+  userAccount.textContent = data.login;
+
+  const userBio = document.querySelector('.user__bio');
+  userBio.textContent = data.bio;
+
+  const repositories = data.repositories.edges;
+
+  const repoCount = document.querySelector('#repo-count');
+  repoCount.textContent = data.repositories.totalCount;
+  
+  
+  
+  
+  loader.style.display = 'none';
 }
 
+loader.style.display = 'flex';
 const githubData = {
   token: 'ef7f55a69a526c830119bf634ccc823338f03ab3',
   username: 'ireade',
@@ -30,6 +64,7 @@ const body = {
             updatedAt
             forkCount
             resourcePath
+            description
             stargazerCount
             primaryLanguage {
               name
@@ -58,6 +93,7 @@ fetch('https://api.github.com/graphql', fetchData)
   populateDOM(user);
 }).catch((err) => {
   console.log(err);
+  loader.style.display = 'none';
 })
 
 const handleScroll = () => {
